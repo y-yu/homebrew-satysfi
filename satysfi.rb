018f1f28bb2fec8247b4fb4818b7f282d07eb27a
Yoshimura Yuu <yyu@mental.poker>
2018-12-08 18:02:59 +0900

    Add .travis.yml

--- satysfi.rb
+++ satysfi.rb
@@ -3,7 +3,7 @@ class Satysfi < Formula
   homepage "https://github.com/gfngfn/SATySFi"
   url "https://github.com/gfngfn/SATySFi/archive/v0.0.2.tar.gz"
   sha256 "91b98654a99d8d13028d4c7334efa9d8cc792949b9ad1be5ec8b4cbacfaea732"
-  head 'https://github.com/gfngfn/SATySFi.git'
+  head "https://github.com/gfngfn/SATySFi.git"
 
   depends_on "ocaml" => :build
   depends_on "opam" => :build
@@ -15,20 +15,30 @@ class Satysfi < Formula
       opamroot.mkpath
       ENV["OPAMROOT"] = opamroot
       ENV["OPAMYES"] = "1"
-      system "opam", "init", "--no-setup"
+      system "opam", "init", "--no-setup", "--disable-sandboxing"
 
-      # The same trick is used by xhyve:
-      # https://github.com/Homebrew/homebrew-core/blob/71576cecdc8bf61550134e56ce837e6fc27dea4a/Formula/docker-machine-driver-xhyve.rb#L47-L48
-      inreplace "#{opamroot}/compilers/4.06.1/4.06.1/4.06.1.comp",
-        '["./configure"', '["./configure" "-no-graph"' # Avoid X11
+      head do
+        system "opam", "switch", "4.07.1"
+      end
 
-      system "opam", "switch", "4.06.1"
+      stable do
+        system "opam", "switch", "4.06.1"
+      end
+      
       system "opam", "config", "exec", "--", "opam", "repository", "add", "satysfi-external", "https://github.com/gfngfn/satysfi-external-repo.git"
       system "opam", "config", "exec", "--", "opam", "pin", "add", "-n", "satysfi", buildpath
       system "opam", "config", "exec", "--", "opam", "install", "satysfi", "--deps-only"
+
+      system "mkdir", "-p", "temp"
+      system "unzip", "-o", "lm2.004otf.zip", "*.otf", "-d", "lib-satysfi/dist/fonts/"
+      system "unzip", "-o", "latinmodern-math-1959.zip", "*.otf", "-d", "temp/"
+      system "cp", "temp/latinmodern-math-1959/otf/latinmodern-math.otf", "lib-satysfi/dist/fonts/"
+      system "unzip", "-o", "junicode-1.002.zip", "*.ttf", "-d", "lib-satysfi/dist/fonts/"
+      system "unzip", "-o", "IPAexfont00301.zip", "*.ttf", "-d", "temp/"
+      system "cp", "temp/IPAexfont00301/ipaexg.ttf", "lib-satysfi/dist/fonts/"
+      system "cp", "temp/IPAexfont00301/ipaexm.ttf", "lib-satysfi/dist/fonts/"
       system "opam", "config", "exec", "--", "make", "-C", buildpath, "PREFIX=#{prefix}"
     end
-    system "make", "lib"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
